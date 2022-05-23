f1955a94f0103625d042cd9e7fa9b7057eac33fd
yui-kitagawa <105733455+yui-kitagawa@users.noreply.github.com>
2022-05-23 23:14:52 +0900

    課題

---
+++ 2-1/main.js
@@ -0,0 +1,11 @@
+document.body.textContent = 'hello';
+document.title = 'JavaScript2-1';
+
+//windowオブジェクトの中身をconsol.logで確認してみます
+console.log(window);
+
+let url = window.location.href;
+console.log(url);
+
+window.alert('Hello World');
+
